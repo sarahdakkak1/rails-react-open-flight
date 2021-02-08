4a8e583646ec74479339c66a3ad22fb176057a6d
sarah dakkak <sarah.dakkak1@gmail.com>
2021-02-08 18:30:08 -0500

    addex index on react

--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Airlines = () => {
-    return <div>This is the Airlines#index view for our app.</div>
+    const [airlines, setAirlines] = useState([])
+
+    useEffect(() => {
+        axios.get('/api/v1/airlines.json')
+        .then( resp => console.log(resp) ) 
+        .catch( resp => console.log(resp) )
+    }, [airlines.length])
+
+    return (
+        <div>This is the Airlines#index view for our app.</div>
+    )
 }
 
 export default Airlines
