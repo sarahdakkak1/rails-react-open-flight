2501ad465b8e822204e69fa33d9798f278693ce9
sarah dakkak <sarah.dakkak1@gmail.com>
2021-02-09 12:45:50 -0500

    added airline function for show page

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
 
-const Airline = () => {
-    return <div>This is the Airlines#show view for our app.</div>
+const Airline = (props) => {
+    const [airline, setAirline] = useState({})
+    const [review, setReview] = useState({})
+
+    useEffect (() => {
+        const slug = props.match.params.slug
+        const url = `/api/v1/airlines/${slug}`
+
+        axios.get(url)
+        .then( resp => setAirline(resp.data) )
+        .catch ( resp => console.log (resp) )
+
+    }, [])
+
+
+
+    return <div className="wrapper">
+        <div className="column">
+            <div className="header"></div>
+            <div className="reviews"></div>
+        </div>
+        <div className="column"></div>
+        <div className="review-form">[Review form goes there]</div>
+
+    </div>
 }
 
 export default Airline
