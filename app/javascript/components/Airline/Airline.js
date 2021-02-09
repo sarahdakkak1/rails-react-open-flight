513128202716969c99c19da6f65a6a90c84621ae
sarah dakkak <sarah.dakkak1@gmail.com>
2021-02-09 12:58:10 -0500

    added useEffect

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,16 +1,22 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import Header from './Header'
 
 const Airline = (props) => {
     const [airline, setAirline] = useState({})
     const [review, setReview] = useState({})
+    const [loader, setLoader] = useState(false)
 
     useEffect (() => {
         const slug = props.match.params.slug
         const url = `/api/v1/airlines/${slug}`
 
         axios.get(url)
-        .then( resp => setAirline(resp.data) )
+        .then( resp => {
+            setAirline(resp.data)
+            setLoaded(true)
+        })
+
         .catch ( resp => console.log (resp) )
 
     }, [])
@@ -19,7 +25,9 @@ const Airline = (props) => {
 
     return <div className="wrapper">
         <div className="column">
-            <div className="header"></div>
+            <Header
+            attributes={airline.data.attributes}
+            />
             <div className="reviews"></div>
         </div>
         <div className="column"></div>
