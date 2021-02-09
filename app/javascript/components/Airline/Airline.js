e3fad17ef732db03fd0a48e929953524aed81567
sarah dakkak <sarah.dakkak1@gmail.com>
2021-02-09 15:46:39 -0500

    Added styled-componennt to show

--- app/javascript/components/Airline/Airline.js
+++ app/javascript/components/Airline/Airline.js
@@ -1,13 +1,34 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import Header from './Header'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    margin-left: auto;
+    margin-right: auto;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+`
+const Column = styled.div`
+    background: #fff;
+    overflow: scroll;
+    height: 100vh;
+
+    &:last-child {
+        background: #000;
+    }
+`
+const Main = styled.div`
+    left-padding: 50px;
+`
+
 
 const Airline = (props) => {
     const [airline, setAirline] = useState({})
     const [review, setReview] = useState({})
-    const [loader, setLoader] = useState(false)
+    const [loaded, setLoaded] = useState(false)
 
-    useEffect (() => {
+    useEffect(() =>{
         const slug = props.match.params.slug
         const url = `/api/v1/airlines/${slug}`
 
@@ -16,24 +37,31 @@ const Airline = (props) => {
             setAirline(resp.data)
             setLoaded(true)
         })
-
-        .catch ( resp => console.log (resp) )
-
+        .catch ( resp => console.log(resp) )
     }, [])
 
 
 
-    return <div className="wrapper">
-        <div className="column">
-            <Header
-            attributes={airline.data.attributes}
-            />
-            <div className="reviews"></div>
-        </div>
-        <div className="column"></div>
-        <div className="review-form">[Review form goes there]</div>
+    return (
+    <Wrapper>
+        <Column>
+        <Main>
 
-    </div>
+            { 
+            loaded &&
+                <Header
+                    attributes={airline.data.attributes}
+                    reviews={airline.included}
+                    />
+                }
+            <div className="reviews"></div>
+        </Main>
+        </Column>
+        <div className="column">
+        <div className="review-form">[Review form goes there]</div>
+        </div>
+    </Wrapper>
+    )
 }
 
 export default Airline
--- app/javascript/components/Airline/Header.js
+++ app/javascript/components/Airline/Header.js
@@ -1,15 +1,19 @@
 import React from 'react'
 
 const Header = (props) => {
- return(
-    <div className="wrapper">
-        <h1><img src="" /></h1>
-    <div>
-    <div className="totalReviews"></div>
-    <div className="starRating"></div>
-    <div className="totalOutOf"></div>
-  </div>
-  </div>
+   const {name, image_url, avg_score} = props.attributes
+   const total = props.reviews.length
+
+
+    return(
+        <div className="wrapper">
+        <h1><img src={image_url} alt={name} />{name}</h1>
+        <div>
+        <div className="totalReviews">{total} User Reviews</div>
+        <div className="starRating"></div>
+        <div className="totalOutOf">{avg_score} 3 out of 5</div>
+    </div>
+    </div>
     )
 }
 
