f73b44417a520401e271903ecfd5dad70e69ef24
sarah dakkak <sarah.dakkak1@gmail.com>
2021-02-09 16:54:08 -0500

    added style to header

--- app/javascript/components/Airline/Header.js
+++ app/javascript/components/Airline/Header.js
@@ -1,5 +1,26 @@
 import React from 'react'
+import Styled from 'styled-components'
 
+const Wrapper = styled.div`
+    padding: 50px 100px 0;
+
+    img {
+        height: 50px;
+        width: 50px;
+        border-radius: 100%;
+        border: 1px solid rgba(0,0,0,1)     
+    }
+
+    h1 {
+        font-size: 30px;
+    }
+`
+const TotalReviews = styled.div`
+    
+`
+const TotalOutOf = styled.div`
+    
+`
 const Header = (props) => {
    const {name, image_url, avg_score} = props.attributes
    const total = props.reviews.length
