1b34968cdf3e55b26ad2ec5a38c9188a5aebf4c6
sarah dakkak <sarah.dakkak1@gmail.com>
2021-02-09 20:15:35 -0500

    added stars components: hover selected and grey

--- app/javascript/components/Airline/ReviewForm.js
+++ app/javascript/components/Airline/ReviewForm.js
@@ -1,4 +1,34 @@
 import React, {Fragment} from 'react'
+import styled from 'styled-components'
+
+const RatingContainer = styled.div`
+    text-align: center;
+    border-radius: 4px;
+    font-size: 18px;
+    padding: 40px 0 10px 0;
+    border: 1px solid #e6e6e6;
+    background: #fff;
+`
+const RatingBox = styled.div`
+    background: #fff;
+    display: flex;
+    justify-content: center;
+    flex-direction: row-reverse;
+    position: relative;
+
+    input {
+        display: none;
+    }
+    
+    label {
+        cursor: pointer;
+        width: 40px;
+        height: 40px;
+        background-image: url();
+    }
+`
+const RatingTitle = styled.div``
+
 
 const ReviewForm = (props) => {
     const ratingOptions = [5,4,3,2,1].map( (score, index) => {
@@ -21,10 +51,12 @@ const ReviewForm = (props) => {
                 <input onChange={props.handleChange} value={props.review.description}  type="text" name="description" placeholder="Review Description"/>
             </div>
             <div className="field">
-                <div className="rating-container">
+                <RatingContainer>
                    <div className="rating-title-text">Rate this Airline</div> 
+                   <div className="rating-box">
                    {ratingOptions}
-                </div>
+                   </div>
+                </RatingContainer>
                 <button type="submit">Submit Your Review</button>
             </div>
         </form>
